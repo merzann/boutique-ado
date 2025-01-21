bce7a5246883339b3c14beed8fc9301bcb484554
Annika Merz <annisch78@gmail.com>
2025-01-21 05:44:54 +0000

    feat: setup allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -81,6 +81,17 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
