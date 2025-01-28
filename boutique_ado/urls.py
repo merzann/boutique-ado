f75f9cb3ebd348e9d424b7e571f957d9a6569c09
Annika Merz <annisch78@gmail.com>
2025-01-28 20:18:46 +0000

    feat: start bag app, add basic view, urls.py and template, add bag uRL to settings.py

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
